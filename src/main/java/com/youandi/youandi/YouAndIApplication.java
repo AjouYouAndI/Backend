eaee178b13743ece44d91225c9914041e416e503
yejipractice <twoye1193@ajou.ac.kr>
2022-05-09 14:14:19 +0900

    [Env] dev setting

---
+++ src/main/java/com/youandi/youandi/YouAndIApplication.java
@@ -0,0 +1,13 @@
+package com.youandi.youandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouAndIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouAndIApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youandi/youandi/YouAndIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youandi.youandi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouAndIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
