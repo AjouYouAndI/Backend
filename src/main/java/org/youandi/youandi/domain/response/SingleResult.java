528225cf335280eb51bbd4596ce8438f709b3a7a
yejipractice <twoye1193@ajou.ac.kr>
2022-05-11 17:00:07 +0900

    [Feat] Response Service setting

---
+++ src/main/java/org/youandi/youandi/domain/response/SingleResult.java
@@ -0,0 +1,10 @@
+package org.youandi.youandi.domain.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SingleResult<T> extends CommonResult {
+    private T data;
+}
